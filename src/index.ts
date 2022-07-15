2a7350ea2d2c390bf733071be09772ecd282472f
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2022-07-14 22:20:55 -0300

    feat: create styles for mask

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
-export function mask(value: string, pattern: string) {
-    let i = 0;
-    let lastReplacedIndex = -1;
+export function mask(value: string, options: { pattern?: string, style?: 'fone' | 'cpf'; }) {
+
 
     const valueSanitized = value.toString().replace(/\W/g, '')
 
-    const result = pattern.replace(/#/g,
+    let formatted = '';
+
+    if (options.pattern) {
+        return replacePattern(options.pattern, valueSanitized)
+    }
+
+    switch (options.style) {
+        case 'fone':
+            return replacePattern('(##) # ####-####', valueSanitized);
+        case 'cpf':
+            return replacePattern('###.###.###-##', valueSanitized);
+    }
+}
+
+function replacePattern(pattern: string, value: string) {
+    let i = 0;
+    let lastReplacedIndex = -1;
+
+    const valueFormatted = pattern.replace(/#/g,
         (_, j) => {
-            if (i >= valueSanitized.length) {
+            if (i >= value.length) {
                 return '#'
             }
             lastReplacedIndex = j;
-            return valueSanitized[i++] || ''
+            return value[i++] || ''
         }
     )
-
-    return result.substring(0, lastReplacedIndex + 1);
+    return valueFormatted.substring(0, lastReplacedIndex + 1);
 }
