f485a5d6b79a6498f70dcb5c687259f4409d4c43
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2022-10-19 11:36:41 -0300

    sannitize time value

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,42 @@
 interface optionsProps {
-    pattern?: string;
-    style?: 'fone' | 'cpf' | 'cep' | 'date' | 'cnpj' | 'time'
+  pattern?: string;
+  style?: "fone" | "cpf" | "cep" | "date" | "cnpj" | "time";
 }
 
 export function mask(value: string, options: optionsProps) {
-    const valueSanitized = value.toString().replace(/\W/g, '')
+  let valueSanitized = value.toString().replace(/\W/g, "");
 
-    if (options.pattern) {
-        return replacePattern(options.pattern, valueSanitized)
-    }
+  if (options.pattern) {
+    return replacePattern(options.pattern, valueSanitized);
+  }
 
-    switch (options.style) {
-        case 'fone':
-            return replacePattern('(##) # ####-####', valueSanitized);
-        case 'cpf':
-            return replacePattern('###.###.###-##', valueSanitized);
-        case 'cep':
-            return replacePattern('#####-###', valueSanitized);
-        case 'date':
-            return replacePattern('##/##/####', valueSanitized);
-        case 'cnpj':
-            return replacePattern('##.###.###/####-##', valueSanitized);
-        case 'time':
-            return replacePattern('##:##:##', valueSanitized);
-    }
+  switch (options.style) {
+    case "fone":
+      return replacePattern("(##) # ####-####", valueSanitized);
+    case "cpf":
+      return replacePattern("###.###.###-##", valueSanitized);
+    case "cep":
+      valueSanitized = valueSanitized.replace(/[^0-9:]/g, "");
+      return replacePattern("#####-###", valueSanitized);
+    case "date":
+      return replacePattern("##/##/####", valueSanitized);
+    case "cnpj":
+      return replacePattern("##.###.###/####-##", valueSanitized);
+    case "time":
+      return replacePattern("##:##:##", valueSanitized);
+  }
 }
 
 function replacePattern(pattern: string, value: string) {
-    let i = 0;
-    let lastReplacedIndex = -1;
+  let i = 0;
+  let lastReplacedIndex = -1;
 
-    const valueFormatted = pattern.replace(/#/g,
-        (_, j) => {
-            if (i >= value.length) {
-                return '#'
-            }
-            lastReplacedIndex = j;
-            return value[i++] || ''
-        }
-    )
-    return valueFormatted.substring(0, lastReplacedIndex + 1);
+  const valueFormatted = pattern.replace(/#/g, (_, j) => {
+    if (i >= value.length) {
+      return "#";
+    }
+    lastReplacedIndex = j;
+    return value[i++] || "";
+  });
+  return valueFormatted.substring(0, lastReplacedIndex + 1);
 }
